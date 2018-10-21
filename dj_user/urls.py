16b656a7bbf65c3e5d778c259286705e92651909
niceShuai <nice_shuai@126.com>
2018-10-22 00:11:50 +0800

    fixed some bugs

--- dj_user/urls.py
+++ dj_user/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     url(r'^register$', views.register, name='register'),
     url(r'^register_handle$', views.register_handle, name='register_handle'),
     url(r'^login$', views.login, name='login'),
-    url(r'^user_center_info$', views.user_center_info),
-    url(r'^user_center_order$', views.user_center_order),
-    url(r'^user_center_site$', views.user_center_site),
+    url(r'^user_center_info$', views.user_center_info, name='user_center_info'),
+    url(r'^user_center_order$', views.user_center_order, name='user_center_order'),
+    url(r'^user_center_site$', views.user_center_site, name='user_center_site'),
 ]
